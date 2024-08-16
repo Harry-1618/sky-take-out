7dd74d96f0ac5d513afd07240144c906f7a55ec2
hrt <1776463307@qq.com>
2024-08-16 15:55:06 +0800

    新增，分页查询，删除套餐业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,70 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * ClassName: SetmealController
+ * Package: com.sky.controller.admin
+ * Description:
+ *
+ * @Author: 黄瑞天
+ * @Create: 2024/8/16 - 15:09
+ * @Version: v1.0
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Slf4j
+@Api(tags = "套餐相关接口")
+public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO){
+        log.info("新增套餐：{}",setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐分页查询
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("套餐分页查询:{}",setmealPageQueryDTO);
+        PageResult pageResult=setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids){
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+}
---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,37 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+/**
+ * ClassName: SetmealService
+ * Package: com.sky.service
+ * Description:
+ *
+ * @Author: 黄瑞天
+ * @Create: 2024/8/16 - 15:13
+ * @Version: v1.0
+ */
+public interface SetmealService {
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+}
