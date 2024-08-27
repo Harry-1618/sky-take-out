d006056fcaf49188bc9a1da644000ae60f4eb482
hrt <1776463307@qq.com>
2024-08-27 19:47:31 +0800

    excel运营数据报表导出业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -96,6 +97,16 @@ public class ReportController {
             LocalDate end){
         log.info("销量排名:{},{}",begin,end);
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    /**
+     * 导出运营报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 
 }
